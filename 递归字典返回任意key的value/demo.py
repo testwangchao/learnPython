10c93f6e4c7dfa7ad97821e0360b8075cc00782c
wangchao <15201403874@163.com>
2017-09-26 10:25:28 +0800

    添加解析字典方法

---
+++ 递归字典返回任意key的value/demo.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+def demo(test_data, key_data):
+    for key, value in test_data.items():
+        if key == key_data:
+            return value
+        else:
+            if isinstance(value, dict):
+                data = demo(value, key_data)
+                return data
+
+
+def main():
+    test = {"a": {"b": 7,"f": {"t": 8}}, "d": {"e": 1}}
+    print demo(test,"t")
+
+if __name__ == '__main__':
+    main()
