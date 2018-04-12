54265199951b225afd7c6697069f58f0efa812cd
wangchao <15201403874@163.com>
2018-04-12 20:20:06 +0800

    s

--- Learnargparse/learnargparse.py
+++ Learnargparse/learnargparse.py
@@ -32,6 +32,8 @@ def return_card(card):
     if type in card_type.keys():
         return card_type.get(type)+number
 
+def return_pai():
+    pass
 
 def main(text):
     data = arg_demo(text)
@@ -39,23 +41,12 @@ def main(text):
     id = demo(user_data,"id")
     card_list = demo(user_data,"card")
     print id
+    card_data = []
     for card in card_list:
         print "".join(return_card(card.get("card")))
-
-
+        # card_data.append(return_card(card.get("card")))
+    # print card_data[0]
 if __name__ == '__main__':
-    # text1 = '''{"msg":{"user":{"id":"1011462","user_login":"test1011462","mobile":null,"password":"qinglong","is_grade":"0","fk":"0","gailv":"0","create_time":"2018-03-21 09:23:11","last_time":"2018-03-21 14:35:05","status":"0","disable_notice":null,"last_login_ip":"117.136.38.134","reg_ip":"106.38.120.114","money":"0","img":"http:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/Q0j4TwGTfTLV9rhlIrTlre0XHIA7ylPb550FuicJ61yWeEUHc3L2XA04UDnjf0J3v6T5QibhJo0kiab9SFnUibvpJA\/132","nickname":"546L6LaF8J+Pgw==","nickname_base64":"546L6LaF8J+Pgw==","token":"c458e91e5ad0d11d916d6f5550e8483d","level":"0","sfgl":"0","openid":"oKAqu08shQyjq6XPRXL40ZFBrPbM","daycard":"0","sex":"1","hyid":"0","history_select":"{\"16\":{\"id\":\"33\",\"rule\":{\"js\":0,\"cm\":0,\"sx\":0}}}","online":1,"zt":1,"room":47616,"card":[{"val":11,"card":"A13","pai":"13","hs":3,"zt":"1","dx":47},{"val":9,"card":"D11","pai":"11","hs":0,"zt":"1","dx":36},{"val":8,"card":"D10","pai":"10","hs":0,"zt":"1","dx":32}],"cardmax":"022201947","typexx":0,"index":0,"dqjf":-8,"djjf":-4,"tpzt":0,"beishu":null,"qbank":null,"niu":null}},"act":"adduser"}'''
-    # text2 = ''''''
-    # text3 = ''''''
-    # text4 = ''''''
-    # text5 = ''''''
-    # text6 = ''''''
-    # text7 = ''''''
-    # text8 = ''''''
-    # text9 = ''''''
-    # all = [text1,text2,text3,text4,text5,text6,text7,text8,text9]
-    # for i in all:
-    #     main(i)
-    with open("C:\\Users\\Administrator\\Desktop\\new3.txt",'r') as file:
+    with open("C:\\Users\\wangchao88\\Desktop\\pai1.txt",'r') as file:
         for i in re.findall('''({"msg":{"user".*?"dx":\d+}],)''',file.read()):
             main(i+"}}}")
